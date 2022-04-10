be96578ec68319db21080776e56a039f6fec3ee3
DatTNT <dattnt24@gmail.com>
2022-04-10 15:13:02 +0700

    JSX, Dynamic, Condition

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
+  const title = "Blog Post";
+  const body = "This is my blog post";
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment one" },
+    { id: 3, text: "Comment one" },
+  ];
+
+  const loading = false;
+  const showComments = true;
+
+  const conmmetBlock = (
+    <div className="comments">
+      <h3>Comments {comments.length}</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>Learn React</a>
-      </header>
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && conmmetBlock}
     </div>
   );
 }
