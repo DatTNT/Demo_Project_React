49ba61eadc30d9fb55b8780b00b6d5834d87d03b
DatTNT <dattnt24@gmail.com>
2022-04-12 11:59:02 +0700

    prop and state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
-import React from "react";
+import Header from "./component/Header";
+import FeedbackItem from "./component/FeedbackItem";
+// function App() {
+//   const title = "Blog Post";
+//   const body = "This is my blog post";
+//   const comments = [
+//     { id: 1, text: "Comment one" },
+//     { id: 2, text: "Comment one" },
+//     { id: 3, text: "Comment one" },
+//   ];
 
+//   const loading = false;
+//   const showComments = true;
+
+//   const conmmetBlock = (
+//     <div className="comments">
+//       <h3>Comments {comments.length}</h3>
+//       <ul>
+//         {comments.map((comment, index) => (
+//           <li key={index}>{comment.text}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+
+//   return (
+//     <div className="container">
+//       <h1>{title.toUpperCase()}</h1>
+//       <p>{body}</p>
+
+//       {showComments && conmmetBlock}
+//     </div>
+//   );
+// }
 function App() {
-  const title = "Blog Post";
-  const body = "This is my blog post";
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment one" },
-    { id: 3, text: "Comment one" },
-  ];
-
-  const loading = false;
-  const showComments = true;
-
-  const conmmetBlock = (
-    <div className="comments">
-      <h3>Comments {comments.length}</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && conmmetBlock}
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem></FeedbackItem>
+      </div>
+    </>
   );
 }
 
